511c0e11f81e1c0e218a392c63cb5a1971f1d2f6
soyoonL <diamomo777@naver.com>
2025-10-28 05:15:36 +0900

    [Feat] 거의 완성

---
+++ GameEngine_Midterm/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;          
+    public float spawnInterval = 10f;        
+    public float spawnRange = 5f;          
+
+    private float timer = 0f;               
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            
+            Vector3 spawnPos = new Vector3(
+                transform.position.x + Random.Range(-spawnRange, spawnRange),
+                transform.position.y,
+                transform.position.z + Random.Range(-spawnRange, spawnRange)
+            );
+
+            Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            timer = 0f;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(spawnRange * 2, 0.1f, spawnRange * 2));
+    }
+}
